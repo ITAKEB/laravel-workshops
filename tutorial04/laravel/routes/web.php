e7bceb20cc7f12fa737810f91cc5e0acff42c0aa
ITAKEB <tomascf123@gmail.com>
2023-03-28 20:19:30 -0500

    Some fixes

---
+++ tutorial04/laravel/routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+Route::get('/about', function () {
+    $data1 = 'About us - Online Store';
+    $data2 = 'About us';
+    $description = 'This is an about page ...';
+    $author = 'Developed by: Your Name';
+
+    return view('home.about')->with('title', $data1)
+      ->with('subtitle', $data2)
+      ->with('description', $description)
+      ->with('author', $author);
+})->name('home.about');
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name('image.index');
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name('image.save');
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name('imagenotdi.index');
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name('imagenotdi.save');
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+  return $request->user();
+});
+
+Route::get('/products', 'App\Http\Controllers\Api\ProductApiController@index')->name('api.product.index');
+Route::get('/products/{id}', 'App\Http\Controllers\Api\ProductApiController@show')->name('api.product.show');
+Route::get('/v2/products', 'App\Http\Controllers\Api\ProductApiControllerV2@index')->name('api.v2.product.index');
+Route::get('/v2/products/{id}', 'App\Http\Controllers\Api\ProductApiControllerV2@show')->name('api.v2.product.show');
+Route::get('/v3/products', 'App\Http\Controllers\Api\ProductApiControllerV3@index')->name('api.v3.product.index');
+Route::get('/v3/products/paginate', 'App\Http\Controllers\Api\ProductApiControllerV3@paginate')->name('api.v3.product.paginate');
+
+Auth::routes();
+
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
